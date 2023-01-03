70522fa35be7fd9f6d2ed585b55059ae8aa35633
MBAFST <mohamed.benamor@etudiant-fst.utm.tn>
2023-01-03 08:02:23 +0100

    Create element and insert to database

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -2,8 +2,32 @@ use sqlx::Pool;
 use sqlx::Postgres;
 
 use crate::model::db::init_db;
-use crate::model::todo::Todo;
-use crate::model::todo::TodoMac;
+
+use super::Todo;
+use super::TodoMac;
+use super::TodoPatch;
+use super::TodoStatus;
+
+#[tokio::test]
+async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
+    // -- FIXTURE
+    let db: Pool<Postgres> = init_db().await?;
+    let data_fx: TodoPatch = TodoPatch {
+        title: Some("test - model_todo_create 1".to_string()),
+        ..Default::default()
+    };
+
+    // -- ACTION
+    let todo_created: Todo = TodoMac::create(&db, data_fx.clone()).await?;
+
+    // -- CHECK
+    println!("\n\n->>{:?}", todo_created);
+    assert!(todo_created.id >= 1000, "Id should be >= 1000");
+    assert_eq!(data_fx.title.unwrap(), todo_created.title);
+    assert_eq!(TodoStatus::Open, todo_created.status);
+
+    return Ok(());
+}
 
 #[tokio::test]
 async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,7 +47,6 @@ async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(100, todos[1].id);
     assert_eq!(123, todos[1].cid);
     assert_eq!("todo 100", todos[1].title);
-    
     
     Ok(())
 }
--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -10,13 +10,40 @@ pub struct Todo {
     pub id: i64,
     pub cid: i64,
     pub title: String,
+    pub status: TodoStatus
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct TodoPatch {
+    pub cid: Option<i64>,
+    pub title: Option<String>,
+    pub status: Option<TodoStatus>
+}
+
+#[derive(sqlx::Type, Debug, Clone, PartialEq, Eq)]
+#[sqlx(type_name="todo_status_enum")]
+#[sqlx(rename_all="lowercase")]
+pub enum TodoStatus {
+    Open,
+    Close
 }
 
 pub struct TodoMac;
 
 impl TodoMac {
+    pub async fn create(db: &Pool<Postgres>, data: TodoPatch) -> Result<Todo, model::Error> {
+        let sql_statement = "insert into todo(cid, title) values($1, $2) returning id, cid, title, status";
+        let query: QueryAs<Postgres, Todo, PgArguments> = sqlx::query_as(&sql_statement)
+            .bind(123 as i64)
+            .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+        
+        let todo: Todo = query.fetch_one(db).await?;
+        
+        return Ok(todo);
+    }
+
     pub async fn list(db: &Pool<Postgres>) -> Result<Vec<Todo>, model::Error>{
-        let sql_statement: &str = "select id, cid, title from todo order by id desc";
+        let sql_statement: &str = "select id, cid, title, status from todo order by id desc";
 
         // Build the sqlx-query
         let query: QueryAs<Postgres, Todo, PgArguments> = sqlx::query_as(&sql_statement);
