e02ec7fd4a93843e2577e44297df6b70ff5545d3
MBAFST <mohamed.benamor@etudiant-fst.utm.tn>
2023-01-03 08:20:16 +0100

    Some changes

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,7 +1,6 @@
+use sqlb::HasFields;
 use sqlx::Pool;
 use sqlx::Postgres;
-use sqlx::query::QueryAs;
-use sqlx::postgres::PgArguments;
 
 use crate::model;
 
@@ -13,9 +12,8 @@ pub struct Todo {
     pub status: TodoStatus
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(sqlb::Fields, Default, Debug, Clone)]
 pub struct TodoPatch {
-    pub cid: Option<i64>,
     pub title: Option<String>,
     pub status: Option<TodoStatus>
 }
@@ -27,28 +25,32 @@ pub enum TodoStatus {
     Open,
     Close
 }
+sqlb::bindable!(TodoStatus);
 
 pub struct TodoMac;
 
 impl TodoMac {
     pub async fn create(db: &Pool<Postgres>, data: TodoPatch) -> Result<Todo, model::Error> {
-        let sql_statement = "insert into todo(cid, title) values($1, $2) returning id, cid, title, status";
-        let query: QueryAs<Postgres, Todo, PgArguments> = sqlx::query_as(&sql_statement)
-            .bind(123 as i64)
-            .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
-        
-        let todo: Todo = query.fetch_one(db).await?;
+        let mut fields = data.fields();
+        fields.push(("cid", 123).into());
+        let sql_builder = sqlb::insert()
+            .table("todo")
+            .data(fields)
+            .returning(&["id", "cid", "title", "status"]);
+
+        let todo: Todo = sql_builder.fetch_one(db).await?;
         
         return Ok(todo);
     }
 
     pub async fn list(db: &Pool<Postgres>) -> Result<Vec<Todo>, model::Error>{
-        let sql_statement: &str = "select id, cid, title, status from todo order by id desc";
+        let sql_builder = sqlb::select()
+            .table("todo")
+            .columns(&["id", "cid", "title", "status"])
+            .order_by("!id");
 
-        // Build the sqlx-query
-        let query: QueryAs<Postgres, Todo, PgArguments> = sqlx::query_as(&sql_statement);
         // Execute the query
-        let todos: Vec<Todo> = query.fetch_all(db).await?;
+        let todos: Vec<Todo> = sql_builder.fetch_all(db).await?;
 
         return Ok(todos);
     }
